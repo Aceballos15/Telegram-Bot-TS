0efd64213d5095df691786955394c15ac7c8caed
Aceballos <ceballoscardonaalexander@gmail.com>
2024-01-29 21:53:43 -0500

    delete urls

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -3,11 +3,9 @@ const axios = require("axios")
 const routers = Router(); 
 
 
-const botToken = "6374276669:AAGx3_lIroSsaNmfxFSlDrQnV1I6UfhxHN4";
-const myWebHook = " https://056b-190-0-247-117.ngrok-free.app/webhook"; 
-
 const botValidations = require("../services/botValidations")
 
+// define principals routers 
 routers.post("/webhook", async (req, res) =>{
     
     const message = req.body; 
